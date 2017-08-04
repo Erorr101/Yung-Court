fc8f3de9a4c2afc860753a7303b551603cdd9742
Junior Heras <xxdragkiller@gmail.com>
2017-08-04 18:28:49 +0000

    badb

--- myscripts.js
+++ myscripts.js
@@ -21,3 +21,34 @@ $('ul li').click(function(){
 });
 
 });
+
+var sidebarBox = document.querySelector('#box'),
+    sidebarBtn = document.querySelector('#btn'),
+    pageWrapper = document.querySelector('#page-wrapper');
+
+sidebarBtn.addEventListener('click', function (event) {
+		sidebarBtn.classList.toggle('active');
+		sidebarBox.classList.toggle('active');
+});
+
+pageWrapper.addEventListener('click', function (event) {
+
+		if (sidebarBox.classList.contains('active')) {
+				sidebarBtn.classList.remove('active');
+				sidebarBox.classList.remove('active');
+		}
+});
+
+window.addEventListener('keydown', function (event) {
+
+		if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+				sidebarBtn.classList.remove('active');
+				sidebarBox.classList.remove('active');
+		}
+});
+    
+    
+  function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}  
